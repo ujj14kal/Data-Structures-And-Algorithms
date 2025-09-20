435a9f33752783cea555ce57651a9235b092c0b3
ujj14kal <ujj.kalra10@gmail.com>
2025-09-20 15:58:16 +0530

    DAY 6

--- dsa.c
+++ dsa.c
@@ -609,3 +609,97 @@ void main (){
     push(sp,29);
     push(sp,26);
 }
+
+
+
+
+//STACK USING LINKED LIST
+
+
+#include<stdio.h>
+#include<stdlib.h>
+ 
+struct Node{
+    int data;
+    struct Node * next;
+};
+ 
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next; 
+    }
+}
+ 
+int isEmpty(struct Node* top){
+    if (top==NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+ 
+int isFull(struct Node* top){
+    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+    if(p==NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+ 
+struct Node* push(struct Node* top, int x){
+    if(isFull(top)){
+        printf("Stack Overflow\n");
+    }
+    else{
+        struct Node* n = (struct Node*) malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        printf("%d added to stack.\n",x);
+    }
+    return top;
+}
+ 
+struct Node* pop(struct Node* tp){
+    if(isEmpty(tp)){
+        printf("Stack Underflow.\n");
+    }
+    else{
+        struct Node* n = tp;
+        tp = (tp)->next;
+        int x = n->data;
+        free(n);
+        printf("%d popped from stack.\n",x);
+        
+    }
+    return tp;
+}
+
+ 
+int main(){
+    struct Node* top = NULL;
+    
+    
+    top = push(top,67);
+    top = push(top,87);
+    top = push(top,97);
+    linkedListTraversal(top);
+    top = pop(top);
+    linkedListTraversal(top);
+    top = pop(top);
+    linkedListTraversal(top);
+    top = push(top,87);
+    linkedListTraversal(top);
+    top = push(top,90);
+    linkedListTraversal(top);
+    top = pop(top);
+    linkedListTraversal(top);
+    
+    return 0;
+}
